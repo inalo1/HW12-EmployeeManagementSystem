da51815a1cb98ceb8c7a411eaaf4897168cdd708
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2020-12-02 16:07:49 -0600

    added schema

---
+++ database/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+
+USE employeeTracker_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+);
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INT,
+    PRIMARY KEY (id)
+);
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ivy Gabrielle", "Nalo", 123, 1)
