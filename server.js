275a2e7f9553e3b9224285afda95428920f62fbf
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2020-12-02 16:43:00 -0600

    added mysql // started 'start' function

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "rootroot",
   database: "employeeTracker_db"
 });
 
@@ -14,3 +14,13 @@ connection.connect(function(err) {
     start();
   });
 
+function start() {
+    inquirer
+        .prompt({
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'choice',
+            choices: ['View all employees', 'View all departments', 'View all roles', 
+            'Add new employee', 'Add new department', 'Add new role', 'Update employee roles']
+        })
+}
